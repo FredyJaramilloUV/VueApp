7056dabbce35fec4eb2ccc8186b104d18d7b974e
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-05-30 17:49:58 -0500

    0.0.2 Cambio de Hola Mundo a Uso de componentes con botones, tarjetas..  On branch main  Changes to be committed: 	modified:   package.json 	modified:   src/App.vue 	new file:   src/components/PostButton.vue 	new file:   src/components/PostCard.vue 	new file:   src/components/PostContainer.vue

---
+++ src/components/PostButton.vue
@@ -0,0 +1,17 @@
+<template>
+<button calss="post-btn" >Acceder</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.post-btn{
+  padding: 15px;
+  background-color: blueviolet;
+  color: white;
+  border: 0;
+  outline: 0;
+}
+</style>
---
+++ src/components/PostContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post-container">
+    <Postcard/>
+    <Postcard/>
+    <Postcard/>
+    <Postcard/>
+  </div>
+</template>
+
+<script setup>
+import Postcard from './PostCard.vue'
+</script>
+
+<style>
+.post-container{
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+</style>
