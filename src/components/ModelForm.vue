3ce6795379cf8151e7a30ac7293357b340b95585
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-06-12 17:10:10 -0500

    V 0.0.9 Feat (todos) Usa input forms check y select con v-model  On branch canvas  Your branch is ahead of 'origin/canvas' by 5 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   src/components/ModelForm.vue
    
     Changes not staged for commit:
    	modified:   src/App.vue
    
     Untracked files:
    	Notas.txt
    DONE: se hizo un input con textbox para probar la funcionalidad de
    v-model
    Se uso un checkbox con un valor por defecto
    Se uso un fomr con un select de varios inputs con valores por defecto  y v-model

---
+++ src/components/ModelForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <input type="text" v-model="inputText">
+    <input type="checkbox" v-model="agree"> Acepto los terminos y condiciones
+    <select v-model="country">
+      <option value="Spain">Spain</option>
+      <option value="UK">UK</option>
+      <option value="France">France</option>
+    </select>
+  </form>
+  <h3>{{ inputText }}</h3>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const country = ref('UK')
+const inputText = ref('')
+const agree = ref(true)
+</script>
+
+<style>
+</style>
