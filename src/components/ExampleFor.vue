eb9e268656ae5236cba6cc638707ccb51a5c8f08
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-06-12 10:23:59 -0500

    V 0.0.6 feat (todos) Ejemplo de v-for  On branch canvas  Your branch is ahead of 'origin/canvas' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   src/components/ExampleFor.vue
    
     Changes not staged for commit:
    	modified:   src/App.vue
    
     Untracked files:
    	Notas.txt
    DONE: ejempplo de uso de la directiva V-for simple
    TODO: probar v-for con objetos como en APIs

---
+++ src/components/ExampleFor.vue
@@ -0,0 +1,13 @@
+<template>
+    <li v-for="country in countries" v-bind:key="country">{{country}} </li>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const countries = ref([
+  'spain', 'France', 'UK', 'Italy', 'portugal'
+])
+</script>
+
+<style scoped>
+</style>
