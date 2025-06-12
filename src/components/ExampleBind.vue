0647ac191336673e1d35241060a2048617e7d807
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-06-12 11:52:49 -0500

    V 0.0.7 feat (todos) Muestra ejemplo bin sobre clase  On branch canvas  Your branch is ahead of 'origin/canvas' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   src/components/ExampleBind.vue
    
     Changes not staged for commit:
    	modified:   src/App.vue
    
     Untracked files:
    	Notas.txt
    DONE: Se hace un estilo de clase condicional a una variable bool usando
    v-bind
    TODO: hacer el otro ejemplo de bind

---
+++ src/components/ExampleBind.vue
@@ -0,0 +1,13 @@
+<template>
+  <p v-bind:class = "{'danger': hasError }"> El mensaje tiene un error si esta rojo </p>
+</template>
+
+<script setup>
+const hasError = true
+</script>
+
+<style>
+.danger{
+  color: red;
+}
+</style>
