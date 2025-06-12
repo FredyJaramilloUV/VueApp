0eda6f099bdc64b1b1c63bb10dbc33acc27e0e00
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-06-12 16:22:25 -0500

    V 0.0.8 feat (todos ) muestra bind style y v-on ademas de @
     On branch canvas
     Your branch is ahead of 'origin/canvas' by 4 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   src/components/ExampleBindEstilos.vue
    
     Changes not staged for commit:
    	modified:   src/App.vue
    
     Untracked files:
    	Notas.txt
    DONE: se hace v-bind con styes, en el modulo anterior se hizo con
    clases. Se uso camell case en lugar de esta_case case para atributos. se
    usa : para reemplazar v-bind. se uso v-on y su reemplazo @ para valores
    de atributos
    TODO: Usar v-model en formularios

---
+++ src/components/ExampleBindEstilos.vue
@@ -0,0 +1,15 @@
+<template>
+  <p v-bind:style = "{'background-color' : '#ccc', color: modoOscuro}">  el mensaje con estilo y uso de v-on @</p>
+    <button v-on:click="handleClick()">Cambiar color</button>
+    <button @click="handleClick()">Cambiar color</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const modoOscuro = ref('#fff')
+const handleClick = () => { modoOscuro.value = '#ff0000' }
+</script>
+
+<style scoped>
+
+</style>
