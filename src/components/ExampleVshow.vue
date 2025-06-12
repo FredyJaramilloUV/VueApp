e5c31b74b8a1a18a14c458720eaf0428db5de163
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-06-12 10:18:55 -0500

    V 0.0.5 feat (todos) muestra ejemplo v-show  On branch canvas  Your branch is ahead of 'origin/canvas' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   src/components/ExampleVshow.vue
    
     Changes not staged for commit:
    	modified:   src/App.vue
    
     Untracked files:
    	ExampleVshow.vue
    	Notas.txt
    	src/components/ExampleFor.vue

---
+++ src/components/ExampleVshow.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 v-show="age < 18">Eres menor de edad</h1>
+  <h1 v-show="age > 18">Eres mayor de edad</h1>
+</template>
+
+<script setup>
+const age = 3
+</script>
+
+<style>
+</style>
